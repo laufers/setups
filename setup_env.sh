3a9bcd807681c8f8ae3bef53a2024dd4487fe867
Mark Laufersweiler <laufers@gmail.com>
2015-09-21 10:42:08 -0500

    added setup script to pull in outside files

---
+++ setup_env.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env sh
+
+# get git-prompt
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+
+# get git-complete
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
