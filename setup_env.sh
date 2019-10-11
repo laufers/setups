59f4fe21df765987671219551beda558504a34b5
Mark Laufersweiler <laufers@ou.edu>
2019-10-11 00:06:25 -0500

    added bash-git-prompt retreived git shell tools

--- setup_env.sh
+++ setup_env.sh
@@ -5,3 +5,6 @@ curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-pro
 
 # get git-complete
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+
+# get bash-git-prompt
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
